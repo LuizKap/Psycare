f0afc5dc37b5923e151f5249bd30d77d4adb3761
Whiskey <luizdetobias@gmail.com>
2026-09-23 18:22:14 -0300

    modificações de erros no authSchema

--- api/src/modules/auth/auth.schema.ts
+++ api/src/modules/auth/auth.schema.ts
@@ -2,26 +2,43 @@ import { z } from 'zod'
 
 export const registerPatientSchema = z.object({
 
-    name: z.string().trim().min(5).max(200).transform(value => value.replace(/\s+/g, ' ')),
+    name: z.string()
+        .trim()
+        .min(5, 'O nome deve ter pelo menos 5 caracteres')
+        .max(200, 'O nome não deve ter mais que 200 caracteres')
+        .transform(value => value.replace(/\s+/g, ' ')),
 
 
-    email: z.email().trim().toLowerCase(),
+    email: z.email({ error: 'Digite um email válido' })
+        .trim()
+        .toLowerCase(),
 
-    phone: z.string().regex(/^\d{11}$/, 'Telefone inválido').optional(),
+    phone: z.string()
+        .regex(/^\d{11}$/, 'Telefone inválido')
+        .optional(),
 
-    password: z.string().min(12).max(100).refine((value) => value.trim().length > 0,
-        { error: 'A senha não pode conter apenas espaços' }),
+    password: z.string()
+        .min(12, 'A senha deve ter no minimo 12 caracteres')
+        .max(100, 'A senha não deve ter mais que 100 caracteres').refine((value) => value.trim().length > 0,
+            { error: 'A senha não pode conter apenas espaços' }),
 
 
     confirmPassword: z.string().min(1, 'O campo não deve ser vazio')
 
 
-}).refine((data => data.password === data.confirmPassword), { error: 'As duas senhas devem ser iguais' }).strict()
+}).refine((data => data.password === data.confirmPassword), {
+
+    error: 'As duas senhas devem ser iguais',
+    path: ['confirmPassword']
+
+}).strict()
 
 
 export const loginPatientSchema = z.object({
 
-    email: z.email().trim().toLowerCase(),
+    email: z.email({ error: 'Digite um email válido' })
+        .trim()
+        .toLowerCase(),
 
     password: z.string().refine((value) => value.trim().length > 0, {
         error: 'A senha não pode conter apenas espaços'
@@ -31,27 +48,40 @@ export const loginPatientSchema = z.object({
 
 export const registerPsychologistSchema = z.object({
 
-    name: z.string().trim().min(5).max(200).transform(value => value.replace(/\s+/g, ' ')),
+    name: z.string()
+        .trim()
+        .min(5, 'O nome deve ter pelo menos 5 caracteres')
+        .max(200, 'O nome não deve ter mais que 200 caracteres')
+        .transform(value => value.replace(/\s+/g, ' ')),
 
 
-    email: z.email().trim().toLowerCase(),
+    email: z.email({ error: 'Digite um email válido' })
+        .trim()
+        .toLowerCase(),
 
 
-    password: z.string().min(12).max(100).refine((value) => value.trim().length > 0,
-        { error: 'A senha não pode conter apenas espaços' }),
+    password: z.string()
+        .min(12, 'A senha deve ter no minimo 12 caracteres')
+        .max(100, 'A senha não deve ter mais que 100 caracteres').refine((value) => value.trim().length > 0,
+            { error: 'A senha não pode conter apenas espaços' }),
 
-    confirmPassword: z.string(),
+    confirmPassword: z.string().min(1, 'O campo não deve ser vazio'),
 
     phone: z.string().regex(/^\d{11}$/, 'Telefone inválido'),
 
     entryCode: z.string().trim().min(1, 'O código é obrigatório')
 
-}).refine((data => data.password === data.confirmPassword), { error: 'As duas senhas devem ser iguais' }).strict()
+}).refine((data => data.password === data.confirmPassword), {
+
+    error: 'As duas senhas devem ser iguais',
+    path: ['confirmPassword']
+
+}).strict()
 
 
 export const loginPsychologistSchema = z.object({
 
-    email: z.email().trim().toLowerCase(),
+    email: z.email({ error: 'Digite um email válido' }).trim().toLowerCase(),
 
     password: z.string().refine((value) => value.trim().length > 0, {
         error: 'A senha não pode conter apenas espaços'
